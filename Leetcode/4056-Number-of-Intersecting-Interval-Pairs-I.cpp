7ade3eb696ad2f94c8f5e88dcd2d6e75afc6107b
Kunal Singh <kunalsingh56737@gmail.com>
2026-09-20 17:04:12 +0530

    [4056] [Time Beats: 32.90%] [Memory Beats: 95.09%] - LeetPush

---
+++ Leetcode/4056-Number-of-Intersecting-Interval-Pairs-I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countIntersectingIntervals(vector<vector<int>>& intervals) {
+        int cnt = 0;
+        int n = intervals.size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (intervals[i][0] <= intervals[j][1] &&
+                    intervals[j][0] <= intervals[i][1]) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
